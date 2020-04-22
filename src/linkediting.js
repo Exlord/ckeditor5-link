3f018eb31e2ddf80ac568b6203ba420422b43c47
Exlord <adeli.farhad@gmail.com>
2020-04-22 09:37:35 +0430

    absolute url

--- src/linkediting.js
+++ src/linkediting.js
@@ -7,19 +7,25 @@
  * @module link/linkediting
  */
 
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import LinkCommand from './linkcommand';
-import UnlinkCommand from './unlinkcommand';
+import Plugin                                                                                          from '@ckeditor/ckeditor5-core/src/plugin';
+import LinkCommand
+																									   from './linkcommand';
+import UnlinkCommand
+																									   from './unlinkcommand';
 import {createLinkElement, ensureAbsolute, ensureSafeUrl, getLocalizedDecorators, normalizeDecorators} from './utils';
-import AutomaticDecorators from './utils/automaticdecorators';
-import ManualDecorator from './utils/manualdecorator';
-import bindTwoStepCaretToAttribute from '@ckeditor/ckeditor5-engine/src/utils/bindtwostepcarettoattribute';
-import findLinkRange from './findlinkrange';
+import AutomaticDecorators
+																									   from './utils/automaticdecorators';
+import ManualDecorator
+																									   from './utils/manualdecorator';
+import bindTwoStepCaretToAttribute
+																									   from '@ckeditor/ckeditor5-engine/src/utils/bindtwostepcarettoattribute';
+import findLinkRange
+																									   from './findlinkrange';
 import '../theme/link.css';
 
-const HIGHLIGHT_CLASS = 'ck-link_selected';
-const DECORATOR_AUTOMATIC = 'automatic';
-const DECORATOR_MANUAL = 'manual';
+const HIGHLIGHT_CLASS       = 'ck-link_selected';
+const DECORATOR_AUTOMATIC   = 'automatic';
+const DECORATOR_MANUAL      = 'manual';
 const EXTERNAL_LINKS_REGEXP = /^(https?:)?\/\//;
 
 /**
@@ -41,12 +47,12 @@ export default class LinkEditing extends Plugin {
 	/**
 	 * @inheritDoc
 	 */
-	constructor( editor ) {
-		super( editor );
+	constructor(editor) {
+		super(editor);
 
-		editor.config.define( 'link', {
+		editor.config.define('link', {
 			addTargetToExternalLinks: false
-		} );
+		});
 	}
 
 	/**
@@ -57,49 +63,55 @@ export default class LinkEditing extends Plugin {
 		const locale = editor.locale;
 
 		// Allow link attribute on all inline nodes.
-		editor.model.schema.extend( '$text', { allowAttributes: 'linkHref' } );
+		editor.model.schema.extend('$text', { allowAttributes: 'linkHref' });
 
-		editor.conversion.for( 'dataDowncast' )
-			.attributeToElement( { model: 'linkHref', view: createLinkElement } );
+		editor.conversion.for('dataDowncast')
+			.attributeToElement({ model: 'linkHref', view: createLinkElement });
 
-		editor.conversion.for( 'editingDowncast' )
-			.attributeToElement( { model: 'linkHref', view: ( href, writer ) => {
-					if(editor.config.get( 'link.forceAbsoluteLink' ))
+		editor.conversion.for('editingDowncast')
+			.attributeToElement({
+				model: 'linkHref',
+				view : (href, writer) => {
+					console.log('href',href);
+					href = ensureSafeUrl(href);
+					if (editor.config.get('link.forceAbsoluteLink'))
 						href = ensureAbsolute(href);
-				return createLinkElement( ensureSafeUrl( href ), writer );
-			} } );
+					return createLinkElement(href, writer);
+				}
+			});
 
-		editor.conversion.for( 'upcast' )
-			.elementToAttribute( {
-				view: {
-					name: 'a',
+		editor.conversion.for('upcast')
+			.elementToAttribute({
+				view : {
+					name      : 'a',
 					attributes: {
 						href: true
 					}
 				},
 				model: {
-					key: 'linkHref',
-					value: viewElement => viewElement.getAttribute( 'href' )
+					key  : 'linkHref',
+					value: viewElement => viewElement.getAttribute('href')
 				}
-			} );
+			});
 
 		// Create linking commands.
-		editor.commands.add( 'link', new LinkCommand( editor ) );
-		editor.commands.add( 'unlink', new UnlinkCommand( editor ) );
+		editor.commands.add('link', new LinkCommand(editor));
+		editor.commands.add('unlink', new UnlinkCommand(editor));
 
-		const linkDecorators = getLocalizedDecorators( editor.t, normalizeDecorators( editor.config.get( 'link.decorators' ) ) );
+		const linkDecorators = getLocalizedDecorators(editor.t,
+			normalizeDecorators(editor.config.get('link.decorators')));
 
-		this._enableAutomaticDecorators( linkDecorators.filter( item => item.mode === DECORATOR_AUTOMATIC ) );
-		this._enableManualDecorators( linkDecorators.filter( item => item.mode === DECORATOR_MANUAL ) );
+		this._enableAutomaticDecorators(linkDecorators.filter(item => item.mode === DECORATOR_AUTOMATIC));
+		this._enableManualDecorators(linkDecorators.filter(item => item.mode === DECORATOR_MANUAL));
 
 		// Enable two-step caret movement for `linkHref` attribute.
-		bindTwoStepCaretToAttribute( {
-			view: editor.editing.view,
-			model: editor.model,
-			emitter: this,
+		bindTwoStepCaretToAttribute({
+			view     : editor.editing.view,
+			model    : editor.model,
+			emitter  : this,
 			attribute: 'linkHref',
 			locale
-		} );
+		});
 
 		// Setup highlight over selected link.
 		this._setupLinkHighlight();
@@ -117,27 +129,27 @@ export default class LinkEditing extends Plugin {
 	 * @private
 	 * @param {Array.<module:link/link~LinkDecoratorAutomaticDefinition>} automaticDecoratorDefinitions
 	 */
-	_enableAutomaticDecorators( automaticDecoratorDefinitions ) {
-		const editor = this.editor;
+	_enableAutomaticDecorators(automaticDecoratorDefinitions) {
+		const editor              = this.editor;
 		const automaticDecorators = new AutomaticDecorators();
 
 		// Adds a default decorator for external links.
-		if ( editor.config.get( 'link.addTargetToExternalLinks' ) ) {
-			automaticDecorators.add( {
-				id: 'linkIsExternal',
-				mode: DECORATOR_AUTOMATIC,
-				callback: url => EXTERNAL_LINKS_REGEXP.test( url ),
+		if (editor.config.get('link.addTargetToExternalLinks')) {
+			automaticDecorators.add({
+				id        : 'linkIsExternal',
+				mode      : DECORATOR_AUTOMATIC,
+				callback  : url => EXTERNAL_LINKS_REGEXP.test(url),
 				attributes: {
 					target: '_blank',
-					rel: 'noopener noreferrer'
+					rel   : 'noopener noreferrer'
 				}
-			} );
+			});
 		}
 
-		automaticDecorators.add( automaticDecoratorDefinitions );
+		automaticDecorators.add(automaticDecoratorDefinitions);
 
-		if ( automaticDecorators.length ) {
-			editor.conversion.for( 'downcast' ).add( automaticDecorators.getDispatcher() );
+		if (automaticDecorators.length) {
+			editor.conversion.for('downcast').add(automaticDecorators.getDispatcher());
 		}
 	}
 
@@ -146,50 +158,56 @@ export default class LinkEditing extends Plugin {
 	 * transforms them into {@link module:link/utils~ManualDecorator} instances and stores them in the
 	 * {@link module:link/linkcommand~LinkCommand#manualDecorators} collection (a model for manual decorators state).
 	 *
-	 * Also registers an {@link module:engine/conversion/downcasthelpers~DowncastHelpers#attributeToElement attribute-to-element}
+	 * Also registers an
+	 * {@link module:engine/conversion/downcasthelpers~DowncastHelpers#attributeToElement attribute-to-element}
 	 * converter for each manual decorator and extends the {@link module:engine/model/schema~Schema model's schema}
 	 * with adequate model attributes.
 	 *
 	 * @private
 	 * @param {Array.<module:link/link~LinkDecoratorManualDefinition>} manualDecoratorDefinitions
 	 */
-	_enableManualDecorators( manualDecoratorDefinitions ) {
-		if ( !manualDecoratorDefinitions.length ) {
+	_enableManualDecorators(manualDecoratorDefinitions) {
+		if (!manualDecoratorDefinitions.length) {
 			return;
 		}
 
-		const editor = this.editor;
-		const command = editor.commands.get( 'link' );
+		const editor           = this.editor;
+		const command          = editor.commands.get('link');
 		const manualDecorators = command.manualDecorators;
 
-		manualDecoratorDefinitions.forEach( decorator => {
-			editor.model.schema.extend( '$text', { allowAttributes: decorator.id } );
+		manualDecoratorDefinitions.forEach(decorator => {
+			editor.model.schema.extend('$text', { allowAttributes: decorator.id });
 
 			// Keeps reference to manual decorator to decode its name to attributes during downcast.
-			manualDecorators.add( new ManualDecorator( decorator ) );
+			manualDecorators.add(new ManualDecorator(decorator));
 
-			editor.conversion.for( 'downcast' ).attributeToElement( {
+			editor.conversion.for('downcast').attributeToElement({
 				model: decorator.id,
-				view: ( manualDecoratorName, writer ) => {
-					if ( manualDecoratorName ) {
-						const attributes = manualDecorators.get( decorator.id ).attributes;
-						const element = writer.createAttributeElement( 'a', attributes, { priority: 5 } );
-						writer.setCustomProperty( 'link', true, element );
+				view : (manualDecoratorName, writer) => {
+					if (manualDecoratorName) {
+						const attributes = manualDecorators.get(
+							decorator.id).attributes;
+						const element    = writer.createAttributeElement(
+							'a', attributes, { priority: 5 });
+						writer.setCustomProperty('link', true,
+							element);
 
 						return element;
 					}
-				} } );
+				}
+			});
 
-			editor.conversion.for( 'upcast' ).elementToAttribute( {
-				view: {
-					name: 'a',
-					attributes: manualDecorators.get( decorator.id ).attributes
+			editor.conversion.for('upcast').elementToAttribute({
+				view : {
+					name      : 'a',
+					attributes: manualDecorators.get(
+						decorator.id).attributes
 				},
 				model: {
 					key: decorator.id
 				}
-			} );
-		} );
+			});
+		});
 	}
 
 	/**
@@ -207,49 +225,50 @@ export default class LinkEditing extends Plugin {
 	 * @private
 	 */
 	_setupLinkHighlight() {
-		const editor = this.editor;
-		const view = editor.editing.view;
+		const editor           = this.editor;
+		const view             = editor.editing.view;
 		const highlightedLinks = new Set();
 
 		// Adding the class.
-		view.document.registerPostFixer( writer => {
+		view.document.registerPostFixer(writer => {
 			const selection = editor.model.document.selection;
-			let changed = false;
+			let changed     = false;
 
-			if ( selection.hasAttribute( 'linkHref' ) ) {
-				const modelRange = findLinkRange( selection.getFirstPosition(), selection.getAttribute( 'linkHref' ), editor.model );
-				const viewRange = editor.editing.mapper.toViewRange( modelRange );
+			if (selection.hasAttribute('linkHref')) {
+				const modelRange = findLinkRange(selection.getFirstPosition(), selection.getAttribute('linkHref'),
+					editor.model);
+				const viewRange  = editor.editing.mapper.toViewRange(modelRange);
 
 				// There might be multiple `a` elements in the `viewRange`, for example, when the `a` element is
 				// broken by a UIElement.
-				for ( const item of viewRange.getItems() ) {
-					if ( item.is( 'a' ) && !item.hasClass( HIGHLIGHT_CLASS ) ) {
-						writer.addClass( HIGHLIGHT_CLASS, item );
-						highlightedLinks.add( item );
+				for (const item of viewRange.getItems()) {
+					if (item.is('a') && !item.hasClass(HIGHLIGHT_CLASS)) {
+						writer.addClass(HIGHLIGHT_CLASS, item);
+						highlightedLinks.add(item);
 						changed = true;
 					}
 				}
 			}
 
 			return changed;
-		} );
+		});
 
 		// Removing the class.
-		editor.conversion.for( 'editingDowncast' ).add( dispatcher => {
+		editor.conversion.for('editingDowncast').add(dispatcher => {
 			// Make sure the highlight is removed on every possible event, before conversion is started.
-			dispatcher.on( 'insert', removeHighlight, { priority: 'highest' } );
-			dispatcher.on( 'remove', removeHighlight, { priority: 'highest' } );
-			dispatcher.on( 'attribute', removeHighlight, { priority: 'highest' } );
-			dispatcher.on( 'selection', removeHighlight, { priority: 'highest' } );
+			dispatcher.on('insert', removeHighlight, { priority: 'highest' });
+			dispatcher.on('remove', removeHighlight, { priority: 'highest' });
+			dispatcher.on('attribute', removeHighlight, { priority: 'highest' });
+			dispatcher.on('selection', removeHighlight, { priority: 'highest' });
 
 			function removeHighlight() {
-				view.change( writer => {
-					for ( const item of highlightedLinks.values() ) {
-						writer.removeClass( HIGHLIGHT_CLASS, item );
-						highlightedLinks.delete( item );
+				view.change(writer => {
+					for (const item of highlightedLinks.values()) {
+						writer.removeClass(HIGHLIGHT_CLASS, item);
+						highlightedLinks.delete(item);
 					}
-				} );
+				});
 			}
-		} );
+		});
 	}
 }
